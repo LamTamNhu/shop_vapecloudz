419450096a9a9c8ebbcd2550cc7ee84d9dfc6464
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-03-27 10:24:01 +0700

    *WIP auth

--- src/main/java/com/shop_vapecloudz/config/JWTTokenHandler.java
+++ src/main/java/com/shop_vapecloudz/config/JWTTokenHandler.java
@@ -2,25 +2,28 @@ package com.shop_vapecloudz.config;
 
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
-@Component
+@Service
 public class JWTTokenHandler {
+    SecretKey key = Jwts.SIG.HS256.key().build();
+
     public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + SecurityConstant.JWT_EXPIRE_TIME);
-        return Jwts.builder().subject(username).issuedAt(currentDate).expiration(expireDate).signWith(Keys.password(SecurityConstant.JWT_SECRET.toCharArray())).compact();
+        return Jwts.builder().subject(username).issuedAt(currentDate).expiration(expireDate).signWith(key).compact();
     }
+
 
     public String getUsernameFromJWT(String token) {
         String jwt;
         try {
-            jwt = Jwts.parser().verifyWith(Keys.password(SecurityConstant.JWT_SECRET.toCharArray())).build().parseSignedClaims(token).getPayload().getSubject();
+            jwt = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
         } catch (JwtException e) {
             e.printStackTrace();
             return null;
@@ -30,7 +33,7 @@ public class JWTTokenHandler {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().verifyWith(Keys.password(SecurityConstant.JWT_SECRET.toCharArray())).build().parseSignedClaims(token);
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
             return true;
         } catch (Exception e) {
             return false;
